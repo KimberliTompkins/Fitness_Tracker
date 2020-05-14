556dd8734dfdd96dabca9718d5aaf60b56105661
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-05-13 19:55:35 -0600

    added app.post(/api/workouts) route to server.js

--- server.js
+++ server.js
@@ -18,6 +18,34 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+// add new exercise
+app.put("/api/workouts/:id", (req, res) => {
+    //const workout = new Workout(req.body);
+
+    //workout.incrementTotalDuration(req.body.duration)
+
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {  $push: { exercises: req.body }},
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
+
+//create new workout
+app.post("/api/workouts", ({ body }, res) => {
+
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
